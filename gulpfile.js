4fe36d5806644791bbbe16417827caee4bde8762
Zheka@RAUSHWEB <rausch08@yandex.ru>
2023-01-30 01:22:03 +0300

    Установка настройка GULP и создание необходимых файлов

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp          = require('gulp');
+const del           = require('del');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass')(require('sass'));
+
+// Сервер
+const server = () => {
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        }
+    });
+};
+
+// Очистка директорий
+const clear = done => {
+    del.sync('./public');
+    done();
+};
+
+// HTML
+const html = () => {
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./public'))
+        .pipe(browserSync.stream());
+};
+
+// SASS
+const scss = () => {
+    return gulp.src('./src/sass/*.{sass,scss}')
+        .pipe(sass())
+        .pipe(gulp.dest('./public'))
+        .pipe(browserSync.stream());
+};
+
+// Слежение за файлами
+const watcher = () => {
+    gulp.watch('./src/*.html', html);
+    gulp.watch('./src/sass/**/*.{sass,scss}', scss);
+};
+
+exports.default = gulp.series(
+    clear,
+    gulp.parallel(html, scss),
+    gulp.parallel(watcher, server)
+);
